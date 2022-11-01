b62b6f1dd975c753f31b29ef16a4a4567cb7c821
nolongernome <lange1995@163.com>
2022-11-01 17:35:42 +0800

    Delete pre.py

--- sample/pre.py
+++
@@ -1,6 +0,0 @@
-import os
-path=os.getcwd()
-for filedir in os.listdir(path):
-    filedir_path = os.path.join(path, filedir)
-    newname=filedir_path.replace("R97","Drugs")
-    os.rename(os.path.join(path, filedir_path), os.path.join(path, newname))
